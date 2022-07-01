97c0634216e3ee0ebfc2d0ea73e96a74f9f572c4
Chris Guida <chrisguida@gmail.com>
2022-07-01 15:41:30 -0500

    revert js properties for now

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,4 +1,3 @@
 export { setConfig } from "./services/setConfig.ts";
 export { dependencies } from "./services/dependencies.ts";
-export { properties } from "./services/properties.ts";
 export { getConfig } from "./services/getConfig.ts";

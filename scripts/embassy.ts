68904f8ef00116e7ccfcce5e520cbb5abd224c2f
Aiden McClelland <3732071+dr-bonez@users.noreply.github.com>
2022-07-15 10:09:30 -0600

    Update manifest.yaml (#66)
    * Update manifest.yaml
    
    * bump version
    
    * update release notes
    
    * added js migration to cover all cases
    
    Co-authored-by: Lucy Cifferello <12953208+elvece@users.noreply.github.com>

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,3 +1,4 @@
 export { setConfig } from "./services/setConfig.ts";
 export { dependencies } from "./services/dependencies.ts";
 export { getConfig } from "./services/getConfig.ts";
+export { migration } from "./services/migrations.ts";

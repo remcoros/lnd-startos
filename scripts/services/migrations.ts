7d6bf7e570f37808dad7a06699deff864321965f
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-07-15 20:40:23 -0600

    fix extra underscore in file name

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -1,7 +1,7 @@
 import { types as T, rangeOf } from "../deps.ts"
 
 import { migration_up_0_14_2_1 } from "../migrations/0_14_2_1__up_migration.ts";
-import { migration_down_0_14_2_1 } from "../migrations/_0_14_2_1_down_migration.ts";
+import { migration_down_0_14_2_1 } from "../migrations/0_14_2_1_down_migration.ts";
 
 // version here is where you are coming from ie. the version the service is currently on
 export const migration: T.ExpectedExports.migration = async (effects, version) => {

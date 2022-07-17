35484b046c254d7498ab4478d0df21f1db71f6d1
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-07-17 06:28:46 -0600

    fix spacing

--- scripts/migrations/0_14_2_1_down_migration.ts
+++ scripts/migrations/0_14_2_1_down_migration.ts
@@ -3,28 +3,28 @@ import { types as T, YAML, matches } from "../deps.ts"
 const { shape, boolean } = matches
 
 const matchTor = shape({
-    tor: shape({
-        "use-tor-only": boolean,
-        "stream-isolation": boolean,
-    })
+  tor: shape({
+    "use-tor-only": boolean,
+    "stream-isolation": boolean,
+  })
 })
 
 export const migration_down_0_14_2_1: T.ExpectedExports.migration = async (effects, _version) => {
-    await effects.createDir({
-        volumeId: "main",
-        path: "start9"
-    })
-    const config = await effects.readFile({
-        volumeId: "main",
-        path: "start9/config.yaml"
-    })
-    const parsed = YAML.parse(config)
+  await effects.createDir({
+    volumeId: "main",
+    path: "start9"
+  })
+  const config = await effects.readFile({
+    volumeId: "main",
+    path: "start9/config.yaml"
+  })
+  const parsed = YAML.parse(config)
 
-    // tor was added in 0.14.2 
-    if (!matchTor.test(parsed)) {
-        return { result: { configured: false } }
-    }
+  // tor was added in 0.14.2 
+  if (!matchTor.test(parsed)) {
+    return { result: { configured: false } }
+  }
 
-    return { result: { configured: true } }
+  return { result: { configured: true } }
 
 }
--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -6,18 +6,18 @@ import { migration_down_0_14_2_1 } from "../migrations/0_14_2_1_down_migration.t
 // version here is where you are coming from ie. the version the service is currently on
 export const migration: T.ExpectedExports.migration = async (effects, version) => {
 
-    // from migrations (upgrades)
-    if (rangeOf('<=0.14.2.1').check(version)) {
-        const result = await migration_up_0_14_2_1(effects,version)
-        return result
-    }
+  // from migrations (upgrades)
+  if (rangeOf('<=0.14.2.1').check(version)) {
+    const result = await migration_up_0_14_2_1(effects,version)
+    return result
+  }
 
-    // to migrations (downgrades)
-    if (rangeOf('>0.14.2.1').check(version)) {
-        const result = await migration_down_0_14_2_1(effects, version)
-        return result
-    }
+  // to migrations (downgrades)
+  if (rangeOf('>0.14.2.1').check(version)) {
+    const result = await migration_down_0_14_2_1(effects, version)
+    return result
+  }
 
-    return { result: { configured: true } }
+  return { result: { configured: true } }
 
 }

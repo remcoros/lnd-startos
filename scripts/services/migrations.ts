91451b3f308858a5ae0cc71a42d584c316da675c
Dread <34528298+islandbitcoin@users.noreply.github.com>
2023-01-27 12:49:34 -0700

    update service name (#94)
    Co-authored-by: islandbitcoin <dread@start9.com>

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -134,5 +134,5 @@ export const migration: T.ExpectedExports.migration = compat.migrations
         down: () => { throw new Error('Cannot downgrade') },
       },
     },
-    "0.15.5",
+    "0.15.5.1",
   );

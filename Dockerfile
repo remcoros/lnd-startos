dcfbed5d50aac2b55499027910b3db1552c39d9c
BitcoinMechanic <45926711+BitcoinMechanic@users.noreply.github.com>
2023-04-28 18:10:22 -0700

    update to 16.2 (#102)
    * update to 16.2
    
    * added migration

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM lightninglabs/lnd:v0.16.1-beta
+FROM lightninglabs/lnd:v0.16.2-beta
 
 ARG ARCH
 ARG PLATFORM
--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -134,5 +134,5 @@ export const migration: T.ExpectedExports.migration = compat.migrations
         down: () => { throw new Error('Cannot downgrade') },
       },
     },
-    "0.16.1",
+    "0.16.2",
   );
